2c88ed7b2a7a6cd58be8e831b9427a16966fccf4
Davy Mitchell <daftspaniel@gmail.com>
2016-09-24 20:20:54 +0100

    Added pipe for Date display. Tweaked colors.

--- lib/document/textdocument.dart
+++ lib/document/textdocument.dart
@@ -1,15 +1,22 @@
 import 'dart:html';
 
 class TextDocument {
-  int id = 1;
+  final int id = 1;
   String text = '';
+  DateTime lastModified;
 
   TextDocument() {
     text = window.localStorage['id1'];
     if (text == null) text = "Welcome to notepad8080";
+    updateModifiedDate();
+  }
+
+  void updateModifiedDate() {
+    lastModified = new DateTime.now();
   }
 
   void save() {
+    updateModifiedDate();
     window.localStorage['id' + id.toString()] = text;
   }
 
--- lib/editor/status_component.dart
+++ lib/editor/status_component.dart
@@ -1,13 +1,32 @@
-import 'package:angular2/core.dart';
 import 'dart:math';
+
+import 'package:angular2/core.dart';
+
 @Component(
     selector: 'text-status',
     templateUrl: 'status_component.html')
 class StatusComponent {
+
   @Input('text')
   String text;
 
+  @Input('modified')
+  DateTime modified;
+
   String get length => text.length.toString();
-  String get wordCount => min(text.split(' ').length, text.length).toString();
-  String get lineCount => ('\n'.allMatches(text).length).toString();
+
+  //String get lastModified => modified.toString();
+
+  String get wordCount {
+    String workingText = text;
+    workingText = workingText.replaceAll('\n', ' ');
+    List<String> words = workingText.split(' ');
+    words.removeWhere((word) => word.length == 0 || word == " ");
+    return min(words.length, text.length).toString();
+  }
+
+  String get lineCount =>
+      ('\n'
+          .allMatches(text)
+          .length).toString();
 }
