609239874035453452697977dba5a1b582fd7790
Davy Mitchell <daftspaniel@gmail.com>
2017-02-20 06:59:16 +0000

    Version bump. App Cache update notification.

--- web/main.dart
+++ web/main.dart
@@ -2,9 +2,20 @@
 
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:html';
+
 import 'package:angular2/platform/browser.dart';
 import 'package:np8080/app_component.dart';
 
+ApplicationCache appCache = window.applicationCache;
+
 main() {
   bootstrap(AppComponent);
+
+  appCache.onUpdateReady.listen((e) => (){
+    if (appCache.status == ApplicationCache.UPDATEREADY) {
+      appCache.swapCache();
+      window.alert('A new version of NP8080 is available. Please reload to enjoy the new hotness!');
+    }
+  });
 }
